d6bf0c1b09363b121fac691f679ab626a784daa3
Marek Lörincz <lorinczdev@gmail.com>
2022-10-17 22:47:52 +0200

    Revert "Add option to ignore missing operators for invalidOperator() method"
    This reverts commit 1500b1ac0034e12b3d7fa8a0661ee405ba7db0a7.

--- src/Models/Query.php
+++ src/Models/Query.php
@@ -367,10 +367,6 @@ class Query
      */
     protected function invalidOperator(mixed $operator): bool
     {
-        if (self::$ignoreMissingOperatos) {
-            return false;
-        }
-
         return ! is_string($operator) || (! in_array(strtolower($operator), $this->operators, true) &&
                 ! in_array(strtolower($operator), $this->grammar->getOperators(), true));
     }
